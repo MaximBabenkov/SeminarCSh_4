b0b08a94e98df32ef031cd894933240544a7d56f
MaximBabenkov <lostbass@mail.ru>
2022-05-26 08:42:28 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу замены элементов массива на противоположные
+int[]array={-1,2,-3,4,5,-6};
+for(int index=0;index<array.Length;index++) Console.Write($"{array[index]} ");
+Console.WriteLine();   
+for(int i=0;i<array.Length;i++) Console.Write($"{array[i]*(-1)} ");
